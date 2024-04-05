9b79e574cd8ed4b024bd6c0273b57dce32715676
Bart Westenenk <bwestenenk@Apple-Centauri.local>
2024-04-05 16:06:49 +0200

    WIP commit

--- ios/Plugin/SumupPlugin.swift
+++ ios/Plugin/SumupPlugin.swift
@@ -5,8 +5,15 @@ import Capacitor
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
+<<<<<<< Updated upstream
 @objc(SumupPlugin)
 public class SumupPlugin: CAPPlugin {
+=======
+@objc(SumupPluginPlugin)
+public class SumupPlugin: CAPPlugin {
+    private let implementation = Sumup()
+
+>>>>>>> Stashed changes
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
         call.resolve([
